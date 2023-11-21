089da4ffa0dc18a472345bbdedc1e4e1169fcfcd
Folayimi <ridwanfolayimi@gmail.com>
2023-11-21 22:02:07 +0100

    Made some changes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-
 // Your javascript goes here
+BrandLogo = document.getElementById("brand-logo");
+
+window.addEventListener("resize", () => {
+  
+  if (window.innerWidth < 768) {
+    BrandLogo.src = "https://crushingit.tech/hackathon-assets/shopify-icon.svg";
+  } else {
+    BrandLogo.src =
+      "https://crushingit.tech/hackathon-assets/shopify-icon-desktop.svg";
+  }
+
+});
+
+
