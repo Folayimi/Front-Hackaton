a7ed7b3cae4466b4778c97a8cad324932a7e3096
Folayimi <ridwanfolayimi@gmail.com>
2023-11-22 00:14:06 +0100

    made changes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // Your javascript goes here
-BrandLogo = document.getElementById("brand-logo");
-Name = document.getElementById("nameCtn");
-NotificationBox = document.getElementById("notif-box");
-ProfileBox = document.getElementById("profile-box");
+const BrandLogo = document.getElementById("brand-logo");
+const Name = document.getElementById("nameCtn");
+const SearchBox = document.getElementById("search-box");
+const NotificationBox = document.getElementById("notif-box");
+const ProfileBox = document.getElementById("profile-box");
+
+if (window.innerWidth < 768) {
+  Name.style.display = "none";
+  BrandLogo.src = "https://crushingit.tech/hackathon-assets/shopify-icon.svg";
+} else {
+  Name.style.display = "block";
+  BrandLogo.src =
+    "https://crushingit.tech/hackathon-assets/shopify-icon-desktop.svg";
+}
 
 window.addEventListener("resize", () => {
   if (window.innerWidth < 768) {
@@ -15,15 +25,25 @@ window.addEventListener("resize", () => {
   }
 });
 
+// SearchBox.addEventListener("mousedown", () => {
+//   console.log("pressed");
+//   SearchBox.style.border = "2px solid var(--White, #fff)";
+// });
+
 NotificationBox.addEventListener("click", () => {
-  console.log("clicked");
   NotificationBox.style.background = "var(--font-mid-2, #616161)";
-  NotificationBox.style.boxShadow =
-    " 0px 0px 0px 3px #005bd3, 0px 0px 0px 1px #000";
+  if (window.innerWidth >= 768) {
+    NotificationBox.style.boxShadow =
+      " 0px 0px 0px 3px #005bd3, 0px 0px 0px 1px #000";
+  }
 });
 
 ProfileBox.addEventListener("click", () => {
-  console.log("clicked");
   ProfileBox.style.background = "var(--font-mid-2, #616161)";
-  ProfileBox.style.boxShadow = " 0px 0px 0px 3px #005bd3, 0px 0px 0px 1px #000";
+  if (window.innerWidth >= 768) {
+    ProfileBox.style.boxShadow =
+      " 0px 0px 0px 3px #005bd3, 0px 0px 0px 1px #000";
+  } else {
+    ProfileBox.style.border = "2px solid #616161";
+  }
 });
